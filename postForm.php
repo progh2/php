46f9e3792bada5e452ea8ec72bfd9f5ec2132265
Gihun Ham <progh2@gmail.com>
2016-01-14 15:34:27 +0900

    #6 POST 연습

---
+++ postForm.php
@@ -0,0 +1,12 @@
+<form method="post" action="postResult.php">
+<h1>For you, VOMVOM service~</h1>
+Input your name<br >
+<input type="text" name="name" id="name" value="">
+<br>
+Input your Birthday(ex:1998 08 07)<br>
+<input type="text" name="year" id="year" value="">
+<input type="text" name="mon" id="mon" value="">
+<input type="text" name="day" id="day" value="">
+<br>
+<input type="submit" value="View My Result" >
+</form>
