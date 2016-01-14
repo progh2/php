9781de53af8aeee07303a78f3d8d544549770dde
Gihun Ham <progh2@gmail.com>
2016-01-14 11:47:59 +0900

    #5 PHP GET 작동연습

---
+++ get.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+
+$goodsid = $_GET['goodsid'];
+if($goodsid == ""){
+  $goodsid = "비어있음";
+}
+
+?>
+
+<a href="./get.php?goodsid=12345" target="_self" > 
+./get.php?goodsid=12345
+</a>
+<hr>
+goodsid는 <?=$goodsid?>입니다.
+<hr>
+현재 넘겨진 값<br >
+<pre>
+<?php print_r($_GET)?>
+</pre>
